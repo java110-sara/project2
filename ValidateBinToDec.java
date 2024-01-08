f919e185912d11df9d6817249b68701e5e321dc7
sara russert <sarabeth.russert@gmail.com>
2024-01-07 18:10:38 -0800

    adds program file and pseudocode

---
+++ ValidateBinToDec.java
@@ -0,0 +1,13 @@
+//name: Sara Russert
+//date: 1/7/24
+//file: ValidateBinToDec.java
+
+// Declare constant for the required binary length
+// Declare all the variables needed by the program
+// Collect user input for the inputString
+// Determine the length of the string; if not correct, exit program
+// Examine each char of the inputString
+// If any char is not valid, flip boolean flag to false
+// If the boolean flag contains true, compute the base ten representation of the binary number
+// else, exit the program
+// Display results to the user
